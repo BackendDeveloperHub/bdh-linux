3e2cf46867d62cfbd942e92ed6744f9a9de2d875
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-04-06 00:50:37 +0530

    Update setup.py

--- cli/bdh_linux/commands/setup.py
+++ cli/bdh_linux/commands/setup.py
@@ -32,7 +32,7 @@ def terminal():
        
     # Powerlevel10k
     if not os.path.isdir(os.path.expanduser("~/.oh-my-zsh/custom/themes/powerlevel10k"))
-         run("git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k")
+         run("git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k"):
     else:
          print("[yellow]⚡ Powerlevel10k already exists, skipping...[/yellow]")
 
